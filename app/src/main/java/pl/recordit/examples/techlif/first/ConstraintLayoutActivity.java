f35af53fc2c78021efd2cf59ff621af7b0552b02
cs <siwon.cezary@gmail.com>
2022-10-04 12:52:05 +0200

    scrollView

--- app/src/main/java/pl/recordit/examples/techlif/first/ConstraintLayoutActivity.java
+++ app/src/main/java/pl/recordit/examples/techlif/first/ConstraintLayoutActivity.java
@@ -1,21 +1,35 @@
 package pl.recordit.examples.techlif.first;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Switch;
 
 public class ConstraintLayoutActivity extends AppCompatActivity {
     private EditText timeEditText;
+    private SwitchCompat switchParent;
+    private RadioGroup radioGroupColors;
+    private Spinner spinnerCity;
+    private Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_constraint_layout);
         timeEditText = findViewById(R.id.editTextTime);
+        switchParent = findViewById(R.id.switch1);
+        radioGroupColors = findViewById(R.id.radioGroup);
+        spinnerCity = findViewById(R.id.spinner);
+        submitButton = findViewById(R.id.button);
+
         timeEditText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -35,6 +49,12 @@ public class ConstraintLayoutActivity extends AppCompatActivity {
             public void afterTextChanged(Editable editable) {
 
             }
+        });
+        submitButton.setOnClickListener(v -> {
+            Log.i("APP", "Is Parent " + switchParent.isChecked());
+            Log.i("APP", "Selected color " + radioGroupColors.getCheckedRadioButtonId());
+            Log.i("APP", "Selected city " + spinnerCity.getSelectedItem().toString());
+
         });
     }
 }
